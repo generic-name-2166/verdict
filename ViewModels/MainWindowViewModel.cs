b571eb7e9af570cc718e1b2458babfbd205ca0cb
generic-name-2166 <kirbesson277@mail.ru>
2024-12-03 01:11:16 +0300

    feat: paginated items

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,51 @@
-﻿using System.Collections.ObjectModel;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Verdict.Models;
 
 namespace Verdict.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    public MainWindowViewModel()
+    {
+        _items =
+        [
+            new TextItemViewModel(new TextItem("Item 1")),
+            new TextItemViewModel(new TextItem("Item 2")),
+        ];
+        _verdicts = new bool?[_items.Length];
+    }
+
+    private readonly TextItemViewModel[] _items;
+    private bool?[] _verdicts;
+
+    // Initializes to a default value of 0
     [ObservableProperty]
-    private ObservableCollection<TextItemViewModel> _items =
-    [
-        new TextItemViewModel(new TextItem("Item")),
-    ];
+    [NotifyPropertyChangedFor(nameof(CurrentItem))]
+    private uint _currentItemIndex;
+
+    public TextItemViewModel CurrentItem => _items[CurrentItemIndex];
+
+    private void RegisterNextItem()
+    {
+        if (CurrentItemIndex < _items.Length - 1)
+            CurrentItemIndex++;
+    }
+
+    public void RegisterYesItem()
+    {
+        _verdicts[CurrentItemIndex] = true;
+        RegisterNextItem();
+    }
+
+    public void RegisterNoItem()
+    {
+        _verdicts[CurrentItemIndex] = false;
+        RegisterNextItem();
+    }
+
+    public void RegisterPreviousItem()
+    {
+        if (CurrentItemIndex > 0)
+            CurrentItemIndex--;
+    }
 }
