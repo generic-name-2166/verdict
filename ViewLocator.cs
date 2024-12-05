7326d94bbca9a931f9505c1df5053f5e2368cdd4
generic-name-2166 <kirbesson277@mail.ru>
2024-12-06 00:42:08 +0300

    chore: reflectionless view locator

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -5,6 +5,7 @@ SPDX-License-Identifier: CC0-1.0
 */
 
 using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using Verdict.ViewModels;
@@ -13,20 +14,29 @@ namespace Verdict;
 
 public class ViewLocator : IDataTemplate
 {
+    private static Dictionary<Type, Func<Control>> _registration = new();
+
+    public static void Register<TViewModel, TView>()
+        where TView : Control, new()
+    {
+        _registration.Add(typeof(TViewModel), () => new TView());
+    }
+
+    public static void Register<TViewModel>(Func<Control> factory)
+    {
+        _registration.Add(typeof(TViewModel), factory);
+    }
+
     public Control? Build(object? param)
     {
         if (param is null)
             return null;
 
-        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
+        var type = param.GetType();
 
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-
-        return new TextBlock { Text = "Not Found: " + name };
+        return _registration.TryGetValue(type, out var factory)
+            ? factory()
+            : new TextBlock { Text = "Not Found: " + type.Name };
     }
 
     public bool Match(object? data)
