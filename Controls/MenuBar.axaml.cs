a6adf1a674e76c419ad6d7517493c005c46d8ad5
generic-name-2166 <kirbesson277@mail.ru>
2024-12-06 01:09:26 +0300

    refactor: set up custom user controls

---
+++ Controls/MenuBar.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Verdict.Controls;
+
+public partial class MenuBarControl : UserControl
+{
+    public MenuBarControl()
+    {
+        InitializeComponent();
+    }
+}
